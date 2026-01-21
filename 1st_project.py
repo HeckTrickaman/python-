9897f564be9f49e7f77edc8732948551a588eb2e
HeckTrickaman <yadaviactaman2030@gmail.com>
2026-01-21 19:13:45 +0530

    Add files via upload

---
+++ 1st_project.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the 1st number = "))
+b=int(input("Enter the 2nd number = "))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("addition = ",add)
+print("subtraction = ",sub)
+print("multiplication = ",mul)
+print("division = ",div)
+
---
+++ 2nd_project.py
@@ -0,0 +1,10 @@
+
+num1=str(input("Enter the first name = "))
+
+
+
+letter= ''' HELLO , </name/>  Welcome  to the python program .
+
+ 
+'''
+print(letter.replace("</name/>",num1))
